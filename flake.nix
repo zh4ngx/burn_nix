cbe570bcc907049d170ac1a20801de85700c43af
Andy Zhang <1329212+zhangbanger@users.noreply.github.com>
2023-12-24 22:37:55 -0800

    Use Burn

--- flake.nix
+++ flake.nix
@@ -1,44 +1,60 @@
 {
-    inputs = {
-        nixpkgs.url= "github:nixos/nixpkgs/nixos-unstable";
-        flake-utils.url = "github:numtide/flake-utils";
-        rust-overlay.url = "github:oxalica/rust-overlay";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
     };
-  description = "A very basic flake";
-
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
-      flake-utils.lib.eachDefaultSystem (system:
-        let
-          overlays = [ (import rust-overlay) ];
-          pkgs = import nixpkgs { inherit system overlays; };
-          rustVersion = pkgs.rust-bin.stable.latest.default;
-          rustPlatform = pkgs.makeRustPlatform {
-            cargo = rustVersion;
-            rustc = rustVersion;
-          };
-          myRustBuild = rustPlatform.buildRustPackage {
-            pname =
-              "burn_nix"; # make this what ever your cargo.toml package.name is
-            version = "0.1.0";
-            src = ./.; # the folder with the cargo.toml
-            nativeBuildInputs = with pkgs; [pkg-config]; # just for the host building the package
-            buildInputs = []; # packages needed by the consumer
-            cargoLock.lockFile = ./Cargo.lock;
-          };
-
-          dockerImage = pkgs.dockerTools.buildImage {
-            name = "rust-burn_nix";
-            config = { Cmd = [ "${myRustBuild}/bin/burn_nix" ]; };
-          };
-        in {
-            packages = {
-                rustPackage = myRustBuild;
-                docker = dockerImage;
-            };
-          defaultPackage = myRustBuild;
-          devShell = pkgs.mkShell {
-            buildInputs =
-              [ (rustVersion.override { extensions = [ "rust-src" ]; }) ];
-          };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+      let 
+        overlays = [ (import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        src = craneLib.cleanCargoSource ./.;
+        nativeBuildInputs = with pkgs; [ rustToolchain pkg-config ];
+        buildInputs = with pkgs; [ sqlite ];
+        commonArgs = {
+          inherit src buildInputs nativeBuildInputs;
+        };
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        bin = craneLib.buildPackage (commonArgs // {
+          inherit cargoArtifacts;
         });
+        dockerImage = pkgs.dockerTools.streamLayeredImage {
+          name = "burn_nix";
+          tag = "latest";
+          contents = [ bin ];
+          config = {
+            Cmd = [ "${bin}/bin/burn_nix" ];
+          };
+        };
+      in
+      with pkgs; 
+        { 
+          packages = {
+            inherit bin dockerImage;
+            default = bin;
+          };
+          devShells.default = mkShell {
+            inputsFrom = [ bin ];
+            buildInputs = with pkgs; [ dive just ];
+          };
+        }
+      );
 }
