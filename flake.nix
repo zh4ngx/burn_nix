93a1cd0f1e3f0ada9316ff82cb839cdb789ae91a
Andy Zhang <1329212+zhangbanger@users.noreply.github.com>
2023-12-24 23:31:29 -0800

    Working shell with LD_LIBRARY_PATH

--- flake.nix
+++ flake.nix
@@ -11,50 +11,47 @@
     };
     crane = {
       url = "github:ipetkov/crane";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs = { nixpkgs.follows = "nixpkgs"; };
     };
   };
   outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-      let 
-        overlays = [ (import rust-overlay)];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile
+          ./rust-toolchain.toml;
         craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
         src = craneLib.cleanCargoSource ./.;
-        nativeBuildInputs = with pkgs; [ rustToolchain pkg-config ];
-        buildInputs = with pkgs; [ sqlite ];
-        commonArgs = {
-          inherit src buildInputs nativeBuildInputs;
-        };
+        nativeBuildInputs = with pkgs; [
+          rustToolchain
+          pkg-config
+          vulkan-loader
+        ];
+        buildInputs = with pkgs; [ ];
+        commonArgs = { inherit src buildInputs nativeBuildInputs; };
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
-        bin = craneLib.buildPackage (commonArgs // {
-          inherit cargoArtifacts;
-        });
+        bin = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
         dockerImage = pkgs.dockerTools.streamLayeredImage {
           name = "burn_nix";
           tag = "latest";
           contents = [ bin ];
           config = {
             Cmd = [ "${bin}/bin/burn_nix" ];
+            Env = [ "LD_LIBRARY_PATH=${pkgs.vulkan-loader}/lib" ];
           };
+
         };
-      in
-      with pkgs; 
-        { 
-          packages = {
-            inherit bin dockerImage;
-            default = bin;
-          };
-          devShells.default = mkShell {
-            inputsFrom = [ bin ];
-            buildInputs = with pkgs; [ dive just ];
-          };
-        }
-      );
+      in with pkgs; {
+        packages = {
+          inherit bin dockerImage;
+          default = bin;
+        };
+        devShells.default = mkShell {
+          inputsFrom = [ bin ];
+          buildInputs = with pkgs; [ dive just ];
+          LD_LIBRARY_PATH = "${pkgs.vulkan-loader}/lib";
+          RUST_BACKTRACE = 1;
+        };
+      });
 }
